04104b763279fdc347888d943423ae28ea124442
ashu-code-alt <ashu.04cap@gmail.com>
2025-07-11 14:00:15 +0200

    Initial website commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Placeholder for future JS functionality
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Vielen Dank! Ihre Nachricht wurde gesendet.');
+});
